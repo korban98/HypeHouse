ff79cee4f5af60cea8f755afca79872868a19d43
Giuseppe Passaro <giusepp.passaro@studenti.unina.it>
2020-01-22 17:38:33 +0100

    versione 1 (connessione)

---
+++ HypeHouse/src/ControllerShop.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class ControllerShop {
+
+	public static void main(String[] args) {
+	//carica il driver
+		try{  
+			Class.forName("com.mysql.cj.jdbc.Driver");  
+			Connection con=DriverManager.getConnection(  
+			"jdbc:mysql://localhost:3306/mydb","root","!abc123!");    
+			Statement stmt=con.createStatement();  
+			stmt.execute("INSERT INTO autore values ('111','luca','rossi')");  
+			//while(rs.next())  
+			//System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
+			//con.close();  
+			}catch(Exception e){ System.out.println(e);}  
+			}  
+			
+	}
