dd45fadf5ee5b23a64a3690786f1958c2f4ff3da
AntonioGiggs <antonio.pascarella6@studenti.unina.it>
2020-01-24 16:07:03 +0100

    new class Utente creation and attributes implementation and add attribute to ControllerShop

--- HypeHouse/src/ControllerShop.java
+++ HypeHouse/src/ControllerShop.java
@@ -4,7 +4,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class ControllerShop {
-
+	private Utente UtenteConnesso;
 	public static void main(String[] args) {
 	//carica il driver
 		try{  
